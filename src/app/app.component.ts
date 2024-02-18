752cff624bf6b86eab2b38ca8a764a4706d23de8
root <root@localhost.localdomain>
2024-02-17 21:12:44 -0500

    camiwa assets added

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -2,6 +2,7 @@ import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import { GlobalService } from './services/global.service';
 import { CommonModule } from '@angular/common';
+import { ScriptService } from './services/script.service';
 @Component({
   selector: 'app-root',
   standalone: true,
@@ -12,8 +13,39 @@ import { CommonModule } from '@angular/common';
 export class AppComponent {
   title = 'camiwa';
   constructor(
-    public global: GlobalService
+    public global: GlobalService,
+    public script: ScriptService
  ) {
- }
+
+  this.script.load(
+    'jquery',
+    'bootstrap',
+    'aos',
+    'macy',
+    'simple-parallax',
+    'owl-carousel',
+    'resizeSensor',
+    'theia-sticky-sidebar',
+    'waypoints',
+    'counter-up',
+    'fancy-ui-widget',
+    'fancy-file-fileupload',
+    'fancy-file-uploader',
+    'fancy-file-transport',
+    'ion.rangeSlider',
+    'magnific-popup',
+    'select2',
+    'google-maps',
+    'custom-script',
+    'listing-map',
+  )
+    .then(() => {
+      console.log('Todos los scripts se cargaron correctamente');
+    })
+    .catch(error => console.log(error));
+
+    // this.epicFunction();
+
+  }
 }
 
