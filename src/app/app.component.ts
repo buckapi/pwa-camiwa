5cc15f51f187454c16e743037226884591cc85d3
root <root@localhost.localdomain>
2024-02-17 20:27:16 -0500

    first commit

---
+++ src/app/app.component.ts
@@ -0,0 +1,19 @@
+import { Component } from '@angular/core';
+import { RouterOutlet } from '@angular/router';
+import { GlobalService } from './services/global.service';
+import { CommonModule } from '@angular/common';
+@Component({
+  selector: 'app-root',
+  standalone: true,
+  imports: [RouterOutlet,CommonModule],
+  templateUrl: './app.component.html',
+  styleUrl: './app.component.css'
+})
+export class AppComponent {
+  title = 'camiwa';
+  constructor(
+    public global: GlobalService
+ ) {
+ }
+}
+
