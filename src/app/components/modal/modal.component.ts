be20041f133159d98cee9dd244f83e815607c488
buckapi <buckapi@localhost.localdomain>
2024-05-11 19:24:50 -0500

    user dasboard

--- src/app/components/modal/modal.component.ts
+++ src/app/components/modal/modal.component.ts
@@ -2,14 +2,21 @@ import { Component } from "@angular/core";
 import { CommonModule } from "@angular/common";
 import { HttpClient } from "@angular/common/http";
 import { NgbModal } from "@ng-bootstrap/ng-bootstrap";
+import { FormsModule } from "@angular/forms";
 @Component({
   selector: 'app-modal',
   standalone: true,
-  imports: [],
+  imports: [
+    CommonModule,
+    FormsModule
+
+  ],
   templateUrl: './modal.component.html',
   styleUrl: './modal.component.css'
 })
 export class ModalComponent {
+  selectedNivel: string = ''; // Propiedad para almacenar el nivel seleccionado
+
 constructor(
   private modalService: NgbModal,
 ){}
